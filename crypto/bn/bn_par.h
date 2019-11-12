7964007a0cecc17ddd5d0e63fc5a631cbc2510c9
Muhammad Reza Ramadhan <rezaramadhan.m@gmail.com>
2019-11-12 10:59:46 +0700

    Add args structure for karatsuba multiplication

---
+++ crypto/bn/bn_par.h
@@ -0,0 +1,15 @@
+#define NUM_THREADS 2
+
+typedef struct _recursive_args_st {
+    BN_ULONG *r;
+    BN_ULONG *a; BN_ULONG *b;
+    int n2;
+    int dna;
+    int dnb;
+    BN_ULONG *t;
+    int used_thr;
+} recursive_args;
+
+// typedef struct _mul_normal_args_st {
+//
+// } mul_normal_args;
